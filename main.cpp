3b2f8a45e65df70885eef11f9d18aa736452e528
Rachel Cameron <rachleecameron@gmail.com>
2021-11-24 02:01:46 -0500

    Commit

---
+++ main.cpp
@@ -0,0 +1,30 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Rachel Cameron
+ */
+
+#include "header.h"
+
+void quadratic_formula(double a, double b, double c){
+   double root1, root2;
+   if((b * b - 4 * a * c) >= 0){
+       root1 = (-b + sqrt(b * b - 4 * a * c))/(2 * a);
+       root2 = (-b - sqrt(b * b - 4 * a * c))/(2 * a);
+       cout<< "Root 1: " << root1 << "\nRoot 2: " << root2;
+   }
+   else{
+       cout << "No real roots.";
+   } 
+}
+
+int main()
+{
+    double a, b, c;
+    cout << "Enter a: " << endl;
+    cin >> a;
+    cout << "Enter b: " << endl;
+    cin >> b;
+    cout << "Enter c: " << endl;
+    cin >> c;
+    quadratic_formula(a, b, c);
+}
